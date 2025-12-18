bbb2b47504a365816b88d5f4913e54578ca3820d
amzi986 <amzarhasan28@gmail.com>
2025-12-18 14:36:00 -0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+st.sidebar.image("assets/Sesamelogo.png")
+st.title("Hello! :wave:", text_alignment= 'left') #
+st.title("Welcome to my presentation!", text_alignment= 'left')
+if st.button("Go to next page"):
+    st.switch_page("pages/Page_2.py")
