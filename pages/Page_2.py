10eabceef9229d97305a4eb803b1e37014c8c5d6
amzi986 <amzarhasan28@gmail.com>
2025-12-18 14:34:01 -0600

    Delete pages/Page_2.py

--- pages/Page_2.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-st.markdown("# OpenSesame is apart of the :rainbow[Future].")
-st.markdown("### The world is ever advancing with technology, and OpenSesame represents a company at the forefront of that progress.")
-st.sidebar.image("—Pngtree—25d business technology computer finance_5780233.png", width=900)
-if st.button("Go to next page"):
-    st.switch_page("pages/Page_3.py")
