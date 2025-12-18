cf1fed5f8d25593045fabc6955ca991c706844e0
amzi986 <amzarhasan28@gmail.com>
2025-12-18 14:31:57 -0600

    Add files via upload

---
+++ pages/Page_5.py
@@ -0,0 +1,32 @@
+import streamlit as st
+st.markdown("# What will I bring?")
+#st.write("")
+#st.write("")
+st.image("assets/Arrow Point Sticker.gif")
+st.write("")
+st.write("")
+st.write("")
+
+
+st.sidebar.image("assets/Love You Computer Sticker.gif")
+
+st.image("assets/Screenshot 2025-12-17 083332.png", width = 10000)
+st.markdown("### I will bring creative problem-solving skills, adaptability, and a strong willingness to learn to OpenSesame.", text_alignment= 'center')
+st.write("")
+st.write("")
+
+st.image("assets/Screenshot 2025-12-17 113520.png")
+#st.markdown("### Fun Fact! This Streamlit presentation is something I built independently for the first time using code! I am super willing to explore new tools, learn new technologies, and turn my ideas into something functional and engaging.")
+#st.markdown("### Through this project, I am able to apply my Python skills that I had learned in class in a practical way while also experimenting with other new things such as design, layout, and user experience, all of which is so fun to do! I am so excited to be able to develop my creative skills with programming with OpenSesame with the incredible resources they provide.")
+st.write("")
+st.write("")
+
+st.markdown("# AND!")
+st.write("")
+st.write("")
+st.image("assets/Screenshot_2025-12-18_124519-removebg-preview.png", width= 1000000)
+st.write("")
+st.write("")
+st.image("assets/Screenshot 2025-12-18 124229.png")
+if st.button("Go to next page"):
+    st.switch_page("pages/Page_6.py")
