cb79629f1759a668bd8e73ab899297ce3ca40dba
amzi986 <amzarhasan28@gmail.com>
2025-12-18 14:28:50 -0600

    Delete pages directory

--- pages/Page_7.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-st.markdown(" # THANK YOU FOR YOUR ATTENTION!!!")
-st.image("assets/Happy Cha Cha GIF by Luke Alexander.gif")
-if st.button("Back to beginning!"):
-     st.switch_page("app.py")
-st.sidebar.image("assets/Good Morning Dancing GIF by Justin Gammon.gif")
