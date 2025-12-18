cf122336ab24f99516f95774db2ad2ecc5085df3
amzi986 <amzarhasan28@gmail.com>
2025-12-18 14:59:36 -0600

    Update app.py

--- app.py
+++ app.py
@@ -3,10 +3,11 @@ st.sidebar.image("assets/Sesamelogo.png", width = 900)
 st.title("Hello! :wave:", text_alignment= 'left') #
 st.title("Welcome to my presentation!", text_alignment= 'left')
 st.write("")
-st.markdown("## Made by Amzar AbidHussain")
+st.markdown("### Made by Amzar AbidHussain")
 st.write("")
 if st.button("Go to next page"):
 
     st.switch_page("pages/Page_2.py")
+
 
 
