3ba257c89f0862b8cfd212c81c898d899227be97
amzi986 <amzarhasan28@gmail.com>
2025-12-18 14:55:35 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import streamlit as st
 st.sidebar.image("assets/Sesamelogo.png", width = 900)
+st.sidebar.markdown("# Made by Amzar AbidHussain")
 st.title("Hello! :wave:", text_alignment= 'left') #
 st.title("Welcome to my presentation!", text_alignment= 'left')
 if st.button("Go to next page"):
 
     st.switch_page("pages/Page_2.py")
+
