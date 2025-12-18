a9464dad529b894ef5dfd9dead582a688c8f9bf0
amzi986 <amzarhasan28@gmail.com>
2025-12-18 14:38:08 -0600

    Update Page_6.py

--- pages/Page_6.py
+++ pages/Page_6.py
@@ -8,4 +8,5 @@ st.image("assets/AI 2.png")
 if st.button("Whats an example of a company that uses AI well?"):
     st.image("assets/OpenSesame_Full_Hor_Grd_Blk.pdf.jpg", width = 100000000)
 if st.button("Next page"):
-     st.switch_page("Page_7.py")
+
+     st.switch_page("pages/Page_7.py")
