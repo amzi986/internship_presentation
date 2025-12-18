9d620e8ee6af25959b93f182e11af7630f0ee284
amzi986 <amzarhasan28@gmail.com>
2025-12-18 14:24:11 -0600

    Add files via upload

---
+++ pages/Page_3.py
@@ -0,0 +1,7 @@
+import streamlit as st
+st.title("How I see OpenSesame fitting into my career is that:")
+st.header("""As an aspiring Software Developer in college, especially at the beginning of my Computer Science journey as a freshman, I am so incredibly eager to pursue this career with my life. I want to get a good head start with my future career and to expand my skills and knowledge in Software Engineering.""")
+st.header("And with OpenSesame, I know my goal will be greatly achieved.")
+if st.button("Go to next page"):
+    st.switch_page("pages/Page_4.py")
+st.sidebar.image("assets/rocket-23413.gif")
